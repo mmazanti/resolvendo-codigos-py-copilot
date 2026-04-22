e07a4e6c17bd3faaff2d04379b3359f7dd2bd49e
mmazanti <math.mazanti@gmail.com>
2026-04-21 23:34:45 -0300

    Update repet_txt.py
    Resolução do desafio de projeto:  repetindo uma string um número x de vezes de uma variável informada.

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,6 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Vamos solicitar uma string e um número inteiro como entrada. Depois teremos que retornar a string o número de vezes informado.
+
+texto = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+print(texto * numero)
