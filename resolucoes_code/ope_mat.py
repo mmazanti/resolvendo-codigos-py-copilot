172b67fb1ffd3948a8b638e7732a064477dd6580
mmazanti <math.mazanti@gmail.com>
2026-04-21 23:36:22 -0300

    Update ope_mat.py
    Resolução do desafio de projeto: declarando e fazendo operações com 2 variáveis.

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,12 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro número: "))
+
+print(f"A soma dos números é: {numero1 + numero2}")
+print(f"A subtração dos números é: {numero1 - numero2}")
+print(f"A multiplicação dos números é: {numero1 * numero2}")
+print(f"A divisão dos números é: {numero1 / numero2}")
+print(f"A exponenciação dos números é: {numero1 ** numero2}")
+print(f"A divisão inteira dos números é: {numero1 // numero2}")
+print(f"O resto da divisão dos números é: {numero1 % numero2}") 
