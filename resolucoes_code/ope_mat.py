6188343bb1d4f34b01f7ece0cf3ff772b963afc6
mmazanti <math.mazanti@gmail.com>
2026-04-21 23:42:33 -0300

    Update ope_mat.py
    Utilizando condicional para melhorar o código e deixa-lo mais interativo.

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -2,11 +2,21 @@
 
 numero1 = int(input("Digite um número: "))
 numero2 = int(input("Digite outro número: "))
+operacao = input('Digite qual a operação que deseja fazer (+ - * / ** // %): ')
 
-print(f"A soma dos números é: {numero1 + numero2}")
-print(f"A subtração dos números é: {numero1 - numero2}")
-print(f"A multiplicação dos números é: {numero1 * numero2}")
-print(f"A divisão dos números é: {numero1 / numero2}")
-print(f"A exponenciação dos números é: {numero1 ** numero2}")
-print(f"A divisão inteira dos números é: {numero1 // numero2}")
-print(f"O resto da divisão dos números é: {numero1 % numero2}") 
+if operacao == '+':
+    print(f"A soma dos números é: {numero1 + numero2}")
+elif operacao == '-':
+    print(f"A subtração dos números é: {numero1 - numero2}")
+elif operacao == '*':
+    print(f"A multiplicação dos números é: {numero1 * numero2}")
+elif operacao == '/':
+    print(f"A divisão dos números é: {numero1 / numero2}")
+elif operacao == '**':
+    print(f"A exponenciação dos números é: {numero1 ** numero2}")
+elif operacao == '//':
+    print(f"A divisão inteira dos números é: {numero1 // numero2}")
+elif operacao == '%':
+    print(f"O resto da divisão dos números é: {numero1 % numero2}") 
+else:
+    print("Operação inválida")
